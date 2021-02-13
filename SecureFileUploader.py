cf9617c72882167b8dd1f57d7d7a14499d349a2a
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-02-13 13:13:42 -0500

    Update SecureFileUploader.py

--- SecureFileUploader.py
+++ SecureFileUploader.py
@@ -1,4 +1,16 @@
+
+
+def file_validation(fname, ftype):
+  allowed_ftype = ['pdf', 'csv', 'docx', 'txt']
+  if fname == "": 
+    return "Failure: No file selected"
+  if ftype not in allowed_ftype:
+    return "Failure: Not allowed file type"
+  return [fname, ftype]
+
 def pdf_upload(fname):
+  if fname == "": return "Failure: No file selected"
+  
   file_path = $WORKSPACE + 'upload/pdf/' + fname
   return Uploadfile(file_path, fname)
 
