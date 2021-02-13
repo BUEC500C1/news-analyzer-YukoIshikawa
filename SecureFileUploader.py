cabdc4592101f22115fbdd44695b60bd7a8a95cc
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-02-13 12:47:11 -0500

    Update and rename SecureFileUploader to SecureFileUploader.py

---
+++ SecureFileUploader.py
@@ -0,0 +1,19 @@
+def pdf_upload(fname):
+  file_path = $WORKSPACE + 'upload/pdf/' + fname
+  return Uploadfile(file_path, fname)
+
+def docx_upload(fname):
+  file_path = $WORKSPACE + 'upload/docx/' + fname
+  return Uploadfile(file_path, fname)
+
+def csv_upload(fname):
+
+def txt_upload(fname):
+
+def upload_status():
+
+def file_upload(fname):
+  
+
+
+
