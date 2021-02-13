1e9e31e091124f03e9aa659d959fbb2238d1c6fa
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-02-13 17:11:58 -0500

    Update stubAPI.py

--- stubAPI.py
+++ stubAPI.py
@@ -1,8 +1,62 @@
+## =============================================
+## Secure File Uploader
+## =============================================
+def Uploadfile():
+      return "Success" 
+
 def pdf_upload(fname):
       file_path = "{/upload/pdf/{filename}}.frame(filename = fname) 
       return Uploadfile(file_path, fname)
 
+def docx_upload(fname):
+      file_path = "{/upload/docx/{filename}}.frame(filename = fname) 
+      return Uploadfile(file_path, fname)
+
+def csv_upload(fname):
+      file_path = "{/upload/csv/{filename}}.frame(filename = fname) 
+      return Uploadfile(file_path, fname)
   
+def txt_upload(fname):
+      file_path = "{/upload/txt/{filename}}.frame(filename = fname) 
+      return Uploadfile(file_path, fname)
+    
+## =============================================
+## News Feed Ingester
+## =============================================
+
+def descover_content(content):
+      return "Found"
+def saveArticle(article):
+      return "Success"
+
+def ingestStatus(content):
+      return discover_content(content)
   
-  
+def NewYorkTimes_ingester(content):
+      return saveArticle(article)
+    
+def WashingtonPost_ingester(content):
+      return saveArticle(article)
+    
+def WallStreetJournal_ingester(content):
+      return saveArticle(article)
+
+## =============================================
+## News Feed Ingester
+## =============================================  
+
+def FindSentiment(sentiment):
+      return "Found"
+def FindeKeyword(keyword):
+      return "Found"
+def FindRelation(relation):
+      return "Found"
       
+def keyword_analysis(keyword):
+    return Findkeyword(keyword)
+  
+def sentiment_analysis(sentiment):
+    return FindSentiment(sentiment):
+    
+def relation_analysis(relation):
+    return FindRelation(relation)
