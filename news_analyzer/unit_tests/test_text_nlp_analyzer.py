6187f667da76f5aab7cfd049fd405f6b974ed145
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-03-01 14:44:38 -0500

    Update test_text_nlp_analyzer.py

--- news_analyzer/unit_tests/test_text_nlp_analyzer.py
+++ news_analyzer/unit_tests/test_text_nlp_analyzer.py
@@ -1,1 +1,25 @@
+from nlp_analyzer import *
+import pytest
 
+def test_getText():
+  assert getText() = [
+{
+ "ID": "",
+ "title": "",
+ "URl" : "",
+ "author" : "", 
+ "publisher" : "",
+ "publication_date" : "",
+ "metadata":"",
+ "NLP_analysis" : "",
+ }
+]
+
+def test_makeNLP():
+  assert makeNLP() = ''
+  
+def test_updateNLP():
+  assert updateNLP() = 'Updated NLP'
+  
+def deleteNLP():
+  assert deleteNLP() = 'Removed NLP'
