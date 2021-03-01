224fe237f2b85b6aa0a3d84e1167b8d981029521
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-03-01 14:34:30 -0500

    Update main.py

--- news_analyzer/text_nlp_analyzer/main.py
+++ news_analyzer/text_nlp_analyzer/main.py
@@ -1,13 +1,34 @@
-texts = [
+from flask import Flask, abort, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# test data 
+NLP = [
 {
- "file_id": "",
+ "file_ID": "",
  "file_name": "",
  "file_URI" : "",
  "file_type" : "", 
- "metadata" : "",
- "author" : "",
- "publisher" : "",
- "publication_date" : "",
- "NLP_analysis" : "",
+ "sentiment" : "",
  }
 ]
+
+@app.route('/', methods=['GET'])
+def getNLP():
+    return jsonify(NLP)
+
+@app.route('/', methods=['POST'])
+def makeNLP():
+   NLP.append(request.get_json())
+   return '', 204
+
+@app.route('/', methods=['PUT'])
+def updateNLP():
+    return 'Updated NLP'
+
+@app.route('/', methods=['DELETE'])
+def deleteNLP(id):
+	delete(NLP)
+    return 'Removed NLP'
