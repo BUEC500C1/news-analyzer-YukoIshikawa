b1130719e192a8af70de4ccce4167677a4147b96
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-03-01 07:06:41 -0500

    Update app.py

--- news_analyzer/app.py
+++ news_analyzer/app.py
@@ -1,1 +1,28 @@
+from flask import Flask
 
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return 'Hello world!'
+
+
+@app.route("/newsfeed_search")
+def newsfeed_search():
+    return 'news feed search'
+
+
+@app.route("/securefile_uploader")
+def secure_file_uploader_ingester():
+    return 'secure_file_uploader_ingester'
+
+
+@app.route("/text_nlp_analyzer")
+def text_nlp_analyzer():
+    return 'text_nlp_analysis'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
