45df0a02c518e4b9fb77e689e30ea59371218594
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-03-01 07:42:44 -0500

    Update main.py

--- news_analyzer/text_nlp_analyzer/main.py
+++ news_analyzer/text_nlp_analyzer/main.py
@@ -1,1 +1,21 @@
 
+Request_headers : {
+    "File_ID" : (file_id, type=str)
+    "File_URI" : (file_uri, type=str)
+    "Text" : (raw.txt, type=array)
+    "Analyses" : (analyses, type=list) {
+        [analysis wanted, type=bool]
+    }
+}
+
+def text_nlp_analysis(Request_headers)
+    Response_headers : {
+        "File_ID" : (file_id, type=str) # given
+        "File_URI" : (file_uri, type=str) # given
+        "Analyses" : (analyses, type=object) { # generated
+            "Sentiment" : (sentiment, type=str)
+            "Keywords" : (keywords, type=list)
+            [other analyses results]
+        }
+    }
+    return Response_headers
