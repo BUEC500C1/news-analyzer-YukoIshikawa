6df1cf2854b0ac1d64b51b73fa76c94cf687a951
Yuko Ishikawa <32304880+YukoIshikawa@users.noreply.github.com>
2021-03-01 07:40:49 -0500

    Update app.py

--- news_analyzer/app.py
+++ news_analyzer/app.py
@@ -8,21 +8,17 @@ api = Api(app)
 def index():
     return 'Hello world!'
 
-
 @app.route("/newsfeed_search")
-def newsfeed_search():
-    return 'news feed search'
-
+def newsfeed_ingester():
+    return 'newsfeed_ingester'
 
 @app.route("/securefile_uploader")
 def secure_file_uploader_ingester():
-    return 'secure_file_uploader_ingester'
-
+    return 'securefile_uploader'
 
 @app.route("/text_nlp_analyzer")
 def text_nlp_analyzer():
-    return 'text_nlp_analysis'
-
+    return 'text_nlp_analyzer'
 
 if __name__ == "__main__":
     app.run(debug=True)
